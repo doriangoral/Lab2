7ea81624d3c41e2a6d705f3c2db7cf787229d4e3
Dorian <dorian.goral@icloud.com>
2023-10-29 09:22:17 +0100

    Zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int integer;
+        double floating;
+        char character;
+        boolean bool;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        integer = scanner.nextInt();
+        System.out.print("Enter a floating point number: ");
+        floating = scanner.nextDouble();
+        System.out.print("Enter a character: ");
+        character = scanner.next().charAt(0);
+        System.out.print("Enter a boolean: ");
+        bool = scanner.nextBoolean();
+
+        //convert double to integer
+        int doubleToInteger = (int) floating;
+        //convert integer to double
+        double integerToDouble = integer;
+        //convert character to integer
+        int characterToInt = Character.getNumericValue(character);
+        //convert boolean to string
+        String boolToString = String.valueOf(bool);
+
+        System.out.println("doubleToInteger: " + doubleToInteger);
+        System.out.println("integerToDouble: " + integerToDouble);
+        System.out.println("characterToInt: " + characterToInt);
+        System.out.println("boolToString: " + boolToString);
         }
 
 }
